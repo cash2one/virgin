ec53dd97539c5bdcfd50c11650c5c20162598aff
hcy <412886356@qq.com>
2016-04-22 09:16:10 +0800

    修改状态

---
+++ test/first_demo.py
@@ -0,0 +1,6 @@
+#--coding:utf-8--#
+_author_='hcy'
+from flask import Blueprint
+
+firstdemo_api = Blueprint('firstdemo_api', __name__)
+
--- virgin.py
+++ virgin.py
@@ -3,13 +3,14 @@ from flask import Flask
 
 from tools.error_warning import error_api
 from test.test import test_api
-
+from test.first_demo import firstdemo_api
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-error_api.register_blueprint(error_api)
-error_api.register_blueprint(test_api)
+app.register_blueprint(error_api)
+app.register_blueprint(test_api)
+app.register_blueprint(firstdemo_api)
 
 if __name__ == '__main__':
     app.run()
