66c34c912e7d28950f68fa0db0f8e1e0f016c9b4
412886356@qq.com <412886356@qq.com>
2016-05-03 06:41:50 +0800

    修改

--- test/autotest.py
+++ test/autotest.py
@@ -1,15 +1,8 @@
-#--coding:utf-8--#
-import jwt
-from flask import Flask,make_response,jsonify
+from flask import Flask, Blueprint
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 
-
-from tools.error_warning import error_api
-from test.test import test_api
-from test.first_demo import firstdemo_api
-from test.autotest import autotest_api
-
+autotest_api = Blueprint('autotest_api', __name__, template_folder='templates')
 
 class User(object):
     def __init__(self, id, username, password):
@@ -28,43 +21,19 @@ users = [
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
 
-
 def authenticate(username, password):
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
-
 
 def identity(payload):
     user_id = payload['identity']
     return userid_table.get(user_id, None)
 
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'super-secret'
+# jwt = JWT(app, authenticate, identity)
 
-app.register_blueprint(error_api)
-app.register_blueprint(test_api)
-app.register_blueprint(firstdemo_api)
-app.register_blueprint(autotest_api)
-
-
-
-
-
-jwt = JWT(app, authenticate, identity)
-
-@app.route('/protected', methods=['POST'])
-@jwt_required()
-def protected():
-    return '%s' % current_identity
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return  make_response(jsonify({'error':'Not found'}),404)
-
-
-if __name__ == '__main__':
-    app.run()
+# @autotest_api.route('/protected')
+# @jwt_required()
+# def protected():
+#     return '%s' % current_identity
