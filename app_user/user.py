5dd272a13776bc681d75b1b0264fb4913b78ee25
hcy <412886356@qq.com>
2016-05-18 18:59:24 +0800

    修改

---
+++ app_user/user.py
@@ -0,0 +1,20 @@
+__author__ = 'hcy'
+from flask import Blueprint,render_template,request
+from connect import conn
+import pymongo
+import tools.tools as tool
+from bson import  json_util
+
+other_api=Blueprint("other_api",__name__,template_folder='templates')
+mongo=conn.mongo_conn_user()
+
+@other_api.route('/usercenter/v1/register/')
+def appversion():
+    item=mongo.android_version.find().sort("addtime",pymongo.DESCENDING)[0]
+    json = {
+            "url": item["url"],
+            "version": item["version"],
+            "describe": item["describe"]
+    }
+    result=tool.return_json(0,"success",json)
+    return json_util.dumps(result,ensure_ascii=False,indent=2)
