f8444f718f046c0f31261d272d805b1e5a495cf9
hcy <412886356@qq.com>
2016-05-19 16:16:38 +0800

    修改

--- connect/conn.py
+++ connect/conn.py
@@ -4,8 +4,8 @@ from flask.ext.pymongo import MongoClient
 import  redis
 
 conn = 'mongodb://FoodMap32loK22Nk3oO_adm9n:UK#45JEIksiJEwi(209Y*nOwm@'
-ip_port0 = '125.211.222.237:27638'
-# ip_port0 = '192.168.22.101:27638'
+#ip_port0 = '125.211.222.237:27638'
+ip_port0 = '192.168.22.101:27638'
 auth = '/db_foodmap?authmechanism=SCRAM-SHA-1'
 db_name = 'db_foodmap'
 
@@ -16,8 +16,8 @@ def mongo_conn():
 
 
 conn_user='mongodb://UserCenter_admin:UserCenter_admin_(*)@'
-ip_port0_user='125.211.222.237:27638'
-# ip_port0_user = '192.168.22.101:27638'
+#ip_port0_user='125.211.222.237:27638'
+ip_port0_user = '192.168.22.101:27638'
 auth_user='/db_UserCenter?authmechanism=SCRAM-SHA-1'
 db_name_user='db_UserCenter'
 
