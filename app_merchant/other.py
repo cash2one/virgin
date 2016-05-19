ebd3bd9aee76cc77796091d107d5c2b636dd60f9
hcy <412886356@qq.com>
2016-05-19 12:34:17 +0800

    修改

--- app_merchant/other.py
+++ app_merchant/other.py
@@ -4,17 +4,23 @@ from connect import conn
 import pymongo
 import tools.tools as tool
 from bson import  json_util
+import auto
 
 other_api=Blueprint("other_api",__name__,template_folder='templates')
 mongo=conn.mongo_conn()
 
-@other_api.route('/fm/merchant/v1/appversion/', methods=['GET'])
+@other_api.route('/fm/merchant/v1/appversion/', methods=['POST'])
 def appversion():
-    item=mongo.android_version.find().sort("addtime",pymongo.DESCENDING)[0]
-    json = {
-            "url": item["url"],
-            "version": item["version"],
-            "describe": item["describe"]
-    }
-    result=tool.return_json(0,"success",json)
+    if request.method == "POST":
+        jwtstr = request.form["jwtstr"]
+    jwtmsg = auto.decodejwt(jwtstr)
+    json={}
+    if jwtmsg:
+        item=mongo.android_version.find().sort("addtime",pymongo.DESCENDING)[0]
+        json = {
+                "url": item["url"],
+                "version": item["version"],
+                "describe": item["describe"]
+        }
+    result=tool.return_json(0,"success",jwtmsg,json)
     return json_util.dumps(result,ensure_ascii=False,indent=2)
