0cd8855c44e10bc6ea0562fdb70c71908a398302
hcy <412886356@qq.com>
2016-05-23 16:22:37 +0800

    修改

--- connect/conn.py
+++ connect/conn.py
@@ -6,7 +6,7 @@ import redis
 _author_ = 'hcy'
 
 conn = 'mongodb://FoodMap32loK22Nk3oO_adm9n:UK#45JEIksiJEwi(209Y*nOwm@'
-#ip_port0 = '125.211.222.237:27638'
+# ip_port0 = '125.211.222.237:27638'
 ip_port0 = '192.168.22.101:27638'
 auth = '/db_foodmap?authmechanism=SCRAM-SHA-1'
 db_name = 'db_foodmap'
@@ -19,7 +19,7 @@ def mongo_conn():
 
 
 conn_user = 'mongodb://UserCenter_admin:UserCenter_admin_(*)@'
-#ip_port0_user='125.211.222.237:27638'
+# ip_port0_user='125.211.222.237:27638'
 ip_port0_user = '192.168.22.101:27638'
 auth_user = '/db_UserCenter?authmechanism=SCRAM-SHA-1'
 db_name_user = 'db_UserCenter'
