20e8640359842f1a3c8f70330296f32cd2b09c5e
送送 <605679688@qq.com>
2016-06-14 15:20:44 +0800

    用户版index

--- tools/tools.py
+++ tools/tools.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import json
 import os
-
+from math import radians, cos, sin, asin, sqrt
 import datetime
 import random
 
@@ -302,6 +302,17 @@ def pimg(uu):
 def gen_rnd_filename():
     filename_prefix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     return '%s%s' % (filename_prefix, str(random.randrange(1000, 10000)))
+def haversine(lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）
+
+    # 将十进制度数转化为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine公式
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # 地球平均半径，单位为公里
+    return c * r * 1000
 if __name__ == '__main__':
     dish = {
                     "is_enabled" : 'str',
