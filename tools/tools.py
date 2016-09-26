eb464a4d8aaffcf5cc5a565db17ed00cb6230999
送送 <605679688@qq.com>
2016-09-26 16:38:16 +0800

    img

--- tools/tools.py
+++ tools/tools.py
@@ -295,10 +295,10 @@ def pimg(uu):
     cc = open(uu, "rb")
     datagen, headers = multipart_encode({"image": cc })
     #发送请求
-    # request = urllib2.Request(settings.setimageIP , datagen, headers)
-    # request = urllib2.Request(settings.setimageIP1 , datagen, headers)
+    request = urllib2.Request(settings.setimageIP , datagen, headers)
+    request = urllib2.Request(settings.setimageIP1 , datagen, headers)
     #测试本地用
-    request = urllib2.Request(settings.setimageIPlocal , datagen, headers)
+    # request = urllib2.Request(settings.setimageIPlocal , datagen, headers)
 
     #获取返回中的内容
     re1 = r"<h1>MD5:(?P<md5>.*?)</h1>"
