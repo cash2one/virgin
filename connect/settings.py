e590c8c06e309e1db01691245c583aff997a5f28
送送 <605679688@qq.com>
2016-11-04 16:18:35 +0800

    img

--- connect/settings.py
+++ connect/settings.py
@@ -1,16 +1,16 @@
 #--coding:utf-8--#
 _author_='hcy'
-getimageIP = 'http://125.211.222.237:17937/'
-setimageIP='http://192.168.22.102:17937'
-setimageIP1='http://192.168.22.102:17937'
-setimageIPlocal = 'http://125.211.222.237:17937/'
+# getimageIP = 'http://125.211.222.237:17937/'
+# setimageIP='http://192.168.22.102:17937'
+# setimageIP1='http://192.168.22.102:17937'
+# setimageIPlocal = 'http://125.211.222.237:17937/'
+#
+# getapk = '125.211.222.237:11087/apk/'
 
-getapk = '125.211.222.237:11087/apk/'
 
-
-# getimageIP = 'http://www.hljpic.cn/'
-# setimageIP='http://pic1.hljpic.cn/'
-# setimageIP1='http://pic2.hljpic.cn/'
+getimageIP = 'http://www.hljpic.cn/'
+setimageIP='http://pic1.hljpic.cn/'
+setimageIP1='http://pic2.hljpic.cn/'
 
 # getapk = 'http://192.168.22.102:17937/apk/'
 
