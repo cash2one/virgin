79f1e45de4e760b7c5a4f8892df6425d29a03dc8
送送 <605679688@qq.com>
2016-11-11 16:35:26 +0800

    img

--- connect/settings.py
+++ connect/settings.py
@@ -1,22 +1,22 @@
 #--coding:utf-8--#
 _author_='hcy'
-getimageIP = 'http://125.211.222.237:17937/'
-setimageIP='http://192.168.22.102:17937'
-setimageIP1='http://192.168.22.102:17937'
-setimageIPlocal = 'http://125.211.222.237:17937/'
-update_img = "/www/site/foodmap/virgin/virgin/static2/upload/"
-tuisongIP = 'http://125.211.222.237:11035'
-SMSnetgate = 'http://125.211.222.232:10031'
+# getimageIP = 'http://125.211.222.237:17937/'
+# setimageIP='http://192.168.22.102:17937'
+# setimageIP1='http://192.168.22.102:17937'
+# setimageIPlocal = 'http://125.211.222.237:17937/'
+# update_img = "/www/site/foodmap/virgin/virgin/static2/upload/"
+# tuisongIP = 'http://125.211.222.237:11035'
+# SMSnetgate = 'http://125.211.222.232:10031'
 
 # getapk = '125.211.222.237:11087/apk/'
 
 
-# getimageIP = 'http://www.hljpic.cn/'
-# setimageIP='http://pic1.hljpic.cn/'
-# setimageIP1='http://pic2.hljpic.cn/'
-# update_img = "/www/site/hallelujiah/virgin/static2/upload/"
-# tuisongIP = 'http://127.0.0.1:10035'
-# SMSnetgate = 'http://125.211.222.232:10031'
+getimageIP = 'http://www.hljpic.cn/'
+setimageIP='http://pic1.hljpic.cn/'
+setimageIP1='http://pic2.hljpic.cn/'
+update_img = "/www/site/hallelujiah/virgin/static2/upload/"
+tuisongIP = 'http://127.0.0.1:10035'
+SMSnetgate = 'http://125.211.222.232:10031'
 
 # getapk = 'http://192.168.22.102:17937/apk/'
 
